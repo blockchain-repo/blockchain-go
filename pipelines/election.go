9c5e7b5346775eae31486a698adf507806088b71
kuro1 <412681778@qq.com>
2017-08-15 11:09:14 +0800

    election pipeline frame

--- pipelines/election.go
+++ pipelines/election.go
@@ -1,1 +1,47 @@
 package pipelines
+
+import (
+	"sync"
+
+	"unichain-go/backend"
+
+	mp "github.com/altairlee/multipipelines/multipipes"
+)
+
+
+func checkForQuorum(arg interface{}) interface{} {
+	return ""
+}
+
+func requeueTransactions(arg interface{}) interface{} {
+	return ""
+}
+
+func createElectionPipe() (p mp.Pipeline) {
+	cvNodeSlice := make([]*mp.Node, 0)
+	cvNodeSlice = append(cvNodeSlice, &mp.Node{Target: checkForQuorum, RoutineNum: 1, Name: "checkForQuorum"})
+	cvNodeSlice = append(cvNodeSlice, &mp.Node{Target: requeueTransactions, RoutineNum: 1, Name: "requeueTransactions"})
+	p = mp.Pipeline{
+		Nodes: cvNodeSlice,
+	}
+	return p
+}
+
+func getElectionChangefeed() mp.Node {
+	conn :=backend.GetConnection()
+	node := mp.Node{
+		Output:conn.ChangefeedRunForever("unichain","vote",1),
+	}
+	return node
+}
+
+func StartElectionPipe() {
+	p := createElectionPipe()
+	changefeed := getElectionChangefeed()
+	p.Setup(&changefeed,nil)
+	p.Start()
+
+	waitRoutine := sync.WaitGroup{}
+	waitRoutine.Add(1)
+	waitRoutine.Wait()
+}
