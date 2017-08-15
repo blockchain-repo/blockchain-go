31857379de75e79b08d7971c85eabfa7ad7738fe
kuro1 <412681778@qq.com>
2017-08-15 09:27:05 +0800

    asset

---
+++ models/asset.go
@@ -0,0 +1,18 @@
+package models
+
+import "unichain-go/common"
+
+type Asset struct {
+	Id         string   `json:"id"`    //
+	Data       map[string]interface{}
+}
+
+func (a *Asset) GenerateId() string {
+	_id := common.GenerateUUID()
+	a.Id = _id
+	return _id
+}
+
+func (a *Asset) ToString() string {
+	return common.Serialize(a)
+}
